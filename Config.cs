b1f46999fbd505dbc31753a0771ab305a1a19855
MonsieurMeh <monsieurmeh@gmail.com>
2025-11-21 22:20:56 -0800

    1.0.0

--- Config.cs
+++ Config.cs
@@ -6,21 +6,55 @@ namespace KnightVSettings;
 public static class Config
 {
     public static ConfigCategory Category { get; private set; }
+    public static ConfigEntry<float> MaxVelocity { get; private set; }
+    public static ConfigEntry<float> MaxPowerUsage { get; private set; }
+    public static ConfigEntry<float> WipeoutMinVelocity { get; private set; }
+    public static ConfigEntry<float> AccelerationInputRate { get; private set; }
+    public static ConfigEntry<float> MaxForwardAcceleration { get; private set; }
+    public static ConfigEntry<float> MaxReverseAcceleration { get; private set; }
+    public static ConfigEntry<float> SlopeHelpMultiplier { get; private set; }
+    public static ConfigEntry<float> LookXRange { get; private set; }
+    public static ConfigEntry<float> LookYRange { get; private set; }
+    public static ConfigEntry<float> JumpForce { get; private set; }
+    public static ConfigEntry<float> JumpDelay { get; private set; }
+    public static ConfigEntry<float> LeanInputRate { get; private set; }
+    public static ConfigEntry<float> LeanFade { get; private set; }
+    public static ConfigEntry<float> LeanVelocityReduction { get; private set; }
+    public static ConfigEntry<float> MinMaxLean { get; private set; }
+    public static ConfigEntry<float> LeanVelocityThreshold { get; private set; }
+    public static ConfigEntry<float> LeanRightLeftBlendSmoothing { get; private set; }
+    public static ConfigEntry<float> LeanRightLeftBlendMinMax { get; private set; }
+    public static ConfigEntry<float> LeanForwardBackwardBlendSmoothing { get; private set; }
+    public static ConfigEntry<float> LeanForwardBackwardBlendMinMax { get; private set; }
+    public static ConfigEntry<float> WaterLevelDismountThreshold { get; private set; }
 
-    //public static ConfigEntry<bool> SomeEntry { get; private set; }
 
-    // Auto populated after calling SettingsRegistry.CreateSettings...
     private static SettingsRegistry.SettingsEntry _settingsEntry;
 
     public static void Init()
     {
-        Category = ConfigSystem.CreateFileCategory("test", "test", "test.cfg");
-
-        // SomeEntry = Category.CreateEntry(
-        //     "some_entry",
-        //     true,
-        //     "Some entry",
-        //     "Some entry that does some stuff.");
+        Category = ConfigSystem.CreateFileCategory("KnightVSettings", "Knight V Settings", "KnightVSettings.cfg");
+        MaxVelocity = Category.CreateEntry("MaxVelocity", 20f, "Max Speed", "Sets maximum speed.");
+        MaxPowerUsage = Category.CreateEntry("MaxPowerUsage", 0f, "Power Usage", "Sets power usage over time");
+        WipeoutMinVelocity = Category.CreateEntry("WipeoutMinVelocity", 1000f, "Wipeout Speed", "Sets minimum speed before wipeout occurs.");
+        AccelerationInputRate = Category.CreateEntry("AccelerationInputRate", 0f, "Rate of Acceleration", "Sets the rate at which acceleration increases or decreases when starting or stopping movement input.");
+        MaxForwardAcceleration = Category.CreateEntry("MaxForwardAcceleration", 15f, "Max Forward Acceleration", "Sets maximum forward acceleration.");
+        MaxReverseAcceleration = Category.CreateEntry("MaxReverseAcceleration", 15f, "Max Reverse Acceleration", "Sets maximum backward acceleration.");
+        SlopeHelpMultiplier = Category.CreateEntry("SlopeHelpMultiplier", 1f, "Slope Assistance", "Multiplier affecting slope movement assistance.");
+        LookXRange = Category.CreateEntry("LookXRange", 135f, "Horizontal Look Range", "Maximum allowed horizontal look angle.");
+        LookYRange = Category.CreateEntry("LookYRange", 120f, "Vertical Look Range", "Maximum allowed vertical look angle.");
+        JumpForce = Category.CreateEntry("JumpForce", 1000f, "Jump Force", "Force applied when jumping.");
+        JumpDelay = Category.CreateEntry("JumpDelay", 0.25f, "Jump Delay", "Delay before jump triggers.");
+        LeanInputRate = Category.CreateEntry("LeanInputRate", 7f, "Lean Input Rate", "Rate at which leaning input is applied.");
+        LeanFade = Category.CreateEntry("LeanFade", 4.5f, "Lean Fade", "How quickly lean input fades when input stops.");
+        LeanVelocityReduction = Category.CreateEntry("LeanVelocityReduction", 0.5f, "Lean Velocity Reduction", "Amount of velocity reduced when leaning.");
+        MinMaxLean = Category.CreateEntry("MinMaxLean", 8f, "Lean Range", "Minimum and maximum allowed lean values.");
+        LeanVelocityThreshold = Category.CreateEntry("LeanVelocityThreshold", 18f, "Lean Velocity Threshold", "Velocity threshold that affects leaning behavior.");
+        LeanRightLeftBlendSmoothing = Category.CreateEntry("LeanRightLeftBlendSmoothing", 2f, "Side Lean Smoothing", "Smoothing factor for left/right lean animation blending.");
+        LeanRightLeftBlendMinMax = Category.CreateEntry("LeanRightLeftBlendMinMax", 0.6f, "Side Lean Range", "Min/max values used for right/left lean blending.");
+        LeanForwardBackwardBlendSmoothing = Category.CreateEntry("LeanForwardBackwardBlendSmoothing", 2f, "Forward Lean Smoothing", "Smoothing factor for forward/backward lean animation blending.");
+        LeanForwardBackwardBlendMinMax = Category.CreateEntry("LeanForwardBackwardBlendMinMax", 0.6f, "Forward Lean Range", "Min/max values used for forward/backward lean blending.");
+        WaterLevelDismountThreshold = Category.CreateEntry("WaterLevelDismountThreshold", 10f, "Water Dismount Level", "Depth of water at which dismount occurs.");
     }
 
     public static void OnSettingsUiClosed()
---
+++ Patch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using Sons.Gameplay;
+using System;
+
+namespace KnightVSettings
+{
+    [HarmonyPatch(typeof(PlayerKnightVAction), "StartRiding")]
+    internal static class KnightVPatch
+    {
+        private static void Postfix(ref PlayerKnightVAction __instance)
+        {
+            __instance._controlDefinition.MaxVelocity = Config.MaxVelocity.Value;
+            __instance._controlDefinition.MaxPowerUsage = Config.MaxPowerUsage.Value;
+            __instance._controlDefinition.WipeoutMinVelocity = Config.WipeoutMinVelocity.Value;
+            __instance._controlDefinition.AccelerationInputRate = Config.AccelerationInputRate.Value;
+            __instance._controlDefinition.MaxForwardAcceleration = Config.MaxForwardAcceleration.Value;
+            __instance._controlDefinition.MaxReverseAcceleration = Config.MaxReverseAcceleration.Value;
+            __instance._controlDefinition.SlopeHelpMultiplier = Config.SlopeHelpMultiplier.Value;
+            __instance._controlDefinition.LookXRange = Config.LookXRange.Value;
+            __instance._controlDefinition.LookYRange = Config.LookYRange.Value;
+            __instance._controlDefinition.JumpForce = Config.JumpForce.Value;
+            __instance._controlDefinition.JumpDelay = Config.JumpDelay.Value;
+            __instance._controlDefinition.LeanInputRate = Config.LeanInputRate.Value;
+            __instance._controlDefinition.LeanFade = Config.LeanFade.Value;
+            __instance._controlDefinition.LeanVelocityReduction = Config.LeanVelocityReduction.Value;
+            __instance._controlDefinition.MinMaxLean = Config.MinMaxLean.Value;
+            __instance._controlDefinition.LeanVelocityThreshold = Config.LeanVelocityThreshold.Value;
+            __instance._controlDefinition.LeanRightLeftBlendSmoothing = Config.LeanRightLeftBlendSmoothing.Value;
+            __instance._controlDefinition.LeanRightLeftBlendMinMax = Config.LeanRightLeftBlendMinMax.Value;
+            __instance._controlDefinition.LeanForwardBackwardBlendSmoothing = Config.LeanForwardBackwardBlendSmoothing.Value;
+            __instance._controlDefinition.LeanForwardBackwardBlendMinMax = Config.LeanForwardBackwardBlendMinMax.Value;
+            __instance._controlDefinition.WaterLevelDismountThreshold = Config.WaterLevelDismountThreshold.Value;
+
+        }
+    }
+}
